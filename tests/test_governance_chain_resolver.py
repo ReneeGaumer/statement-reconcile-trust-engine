63415a7480906a8db5a5ab9feb357d6855fa18f8
ReneeGaumer <david.gaumer@yahoo.com>
2026-06-09 12:30:48 -0500

    Use rule governance repositories in resolver tests

--- tests/test_governance_chain_resolver.py
+++ tests/test_governance_chain_resolver.py
@@ -2,13 +2,15 @@ from datetime import datetime, UTC
 
 from trust_engine.application.governance_chain_resolver import GovernanceChainResolver
 from trust_engine.domain.authoritative_models import RuleVersionRecord, RuleApprovalRecord, RuleGovernanceRecord
-from trust_engine.infrastructure.authoritative_repository import AuthoritativeRepository
+from trust_engine.infrastructure.rule_version_repository import RuleVersionRepository
+from trust_engine.infrastructure.rule_approval_repository import RuleApprovalRepository
+from trust_engine.infrastructure.rule_governance_repository import RuleGovernanceRepository
 
 
 def test_governance_chain_resolver_authorizes_stored_chain():
-    rule_repo = AuthoritativeRepository("rule_version_id")
-    approval_repo = AuthoritativeRepository("approval_id")
-    governance_repo = AuthoritativeRepository("governance_id")
+    rule_repo = RuleVersionRepository()
+    approval_repo = RuleApprovalRepository()
+    governance_repo = RuleGovernanceRepository()
 
     rule = RuleVersionRecord("TRUST_MODEL_RULES_V1", "TRUST_MODEL_RULES", "ACTIVE", datetime.now(UTC), "RULE_FP", None)
     approval = RuleApprovalRecord("APPROVAL-001", "TRUST_MODEL_RULES_V1", "GOVERNANCE_AUTHORITY", datetime.now(UTC), "APPROVED")
@@ -26,9 +28,9 @@ def test_governance_chain_resolver_authorizes_stored_chain():
 
 
 def test_governance_chain_resolver_rejects_missing_governance_record():
-    rule_repo = AuthoritativeRepository("rule_version_id")
-    approval_repo = AuthoritativeRepository("approval_id")
-    governance_repo = AuthoritativeRepository("governance_id")
+    rule_repo = RuleVersionRepository()
+    approval_repo = RuleApprovalRepository()
+    governance_repo = RuleGovernanceRepository()
 
     rule = RuleVersionRecord("TRUST_MODEL_RULES_V1", "TRUST_MODEL_RULES", "ACTIVE", datetime.now(UTC), "RULE_FP", None)
     approval = RuleApprovalRecord("APPROVAL-001", "TRUST_MODEL_RULES_V1", "GOVERNANCE_AUTHORITY", datetime.now(UTC), "APPROVED")
